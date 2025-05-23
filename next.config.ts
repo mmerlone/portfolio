1443519ab35f3466912e12cd369d500e7b6021d5
Marcio Merlone <mmerlone@gmail.com>
2025-05-23 11:31:16 -0300

    chore: config tailwind to use online icons

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,31 @@ const nextConfig: NextConfig = {
         hostname: 'cdn.jsdelivr.net',
         pathname: '/gh/devicons/devicon/icons/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.vectorlogo.zone',
+        pathname: '/logos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'phpipam.net',
+        pathname: '/wp-content/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.bacula.org',
+        pathname: '/wp-content/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '/devicons/devicon/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '/cypress-io/cypress-icons/**',
+      },
     ],
   },
 };
