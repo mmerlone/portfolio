5db308060b6365280b6b0dd845d0685706cae565
Marcio Merlone <mmerlone@gmail.com>
2026-03-30 16:47:35 -0300

    fix: remove className from quote props

--- src/components/QuoteSection.tsx
+++ src/components/QuoteSection.tsx
@@ -3,19 +3,12 @@ import Quote, {
   QuoteFallback,
   type QuoteProps,
 } from "@/components/widgets/Quote";
-import { cn } from "@/lib/cn";
 
-const QuoteSection = ({
-  className = "",
-  quotesPromise,
-}: QuoteProps): ReactElement => {
+const QuoteSection = ({ quotesPromise }: QuoteProps): ReactElement => {
   return (
     <section
       id="quote"
-      className={cn(
-        "relative flex items-center justify-center overflow-hidden bg-gray-100 dark:bg-gray-950",
-        className,
-      )}
+      className="relative flex items-center justify-center overflow-hidden bg-gray-100 dark:bg-gray-950"
     >
       <div className="flex w-full flex-col gap-8 lg:flex-row">
         <div className="m-2 flex w-full items-center justify-center">
